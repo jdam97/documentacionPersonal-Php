a63f4c949c6d43b0e03ae956672b8a879af10187
JonathanDavidAlvarezMonsalveCAMPUS <jonathandalvarez7@gmail.com>
2023-05-29 17:11:45 -0500

    feat: :sparkles: punto 6 imprimir
    agrego el punto 5 que consiste en los métodos para mostrar en el documento

--- index.php
+++ index.php
@@ -1,5 +1,14 @@
 <?php 
-    //Punto 5
-  echo 'Primer commit';
+//Punto 6: Funciones de salida en Php
+    echo 'Hola Mundo!';
+
+    // printf(): Esta función es similar a echo(), pero solo puede imprimir una cadena de texto a la vez
+    $texto = 'Mundo';
+    printf('Hola %s',$texto);
+
+    //sprintf(): Esta función es similar a printf(), pero en lugar de imprimir la cadena formateada en la salida estándar, devuelve la cadena formateada como resultado.
+    $mensaje = sprintf('Hola %s', $texto);
+    echo $mensaje;
+//
    
 ?>
