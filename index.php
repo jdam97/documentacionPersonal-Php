ad2dba6e35a6fe82bdb143f8374f5072587fc9bf
JonathanDavidAlvarezMonsalveCAMPUS <jonathandalvarez7@gmail.com>
2023-06-30 08:29:12 -0500

    feat: :sparkles: punto 16
    Herencia

--- index.php
+++ index.php
@@ -1,40 +1,39 @@
 <?php
-/**
- * todo PHP INTERMEDIO
- */
-POO -> CLASS
-class Cocodrile
-{
-    private $nombre;
-    protected $edad;
+//-----------Herencia POO------------
 
-        public function __construct($nombre, $edad)
+class Transporte
+{
+    public function __construct(protected int $ruedas, protected int $capacidad)
     {
-        $this->nombre = $nombre;
-        $this->edad = $edad;
     }
-    public function getNombre()
+    public function getInfo(): string
     {
-        return $this->nombre;
+        return "El transporte tiene " . $this->ruedas . " ruedas y una capacidad de " . $this->capacidad . " personas.";
     }
-    public function setNombre($nombre)
+    public function getRuedas(): int
     {
-        $this->nombre = $nombre;
-    }
-    public function getEdad()
-    {
-        return $this->edad;
-    }
-    public function setEdad($edad)
-    {
-        $this->edad = $edad;
-    }
-    public function saludar()
-    {
-        echo "Hola, mi nombre es " . $this->nombre . " y mi edad es: " . $this->edad;
+        return $this->ruedas;
     }
 }
-$alumno = new Cocodrile("Chancho", 25);
-$alumno->getNombre(); //Se omite con el constructor
-$alumno->getEdad(); //Se omite con el constructor
-$alumno->saludar();
+class Bicicleta extends Transporte
+{
+    public function getInfo(): string
+    {
+        return "El transporte tiene " . $this->ruedas . " ruedas y una capacidad de " . $this->capacidad . " personas Y NO GASTA GASOLINA.";
+    }
+}
+class Automovil extends Transporte
+{
+    public function __construct(protected int $ruedas, protected int $capacidad, protected string $transmision)
+    {
+    }
+    public function getTransmision()
+    {
+        return $this->transmision;
+    }
+}
+$mercedes = new Automovil(4, 5, "Manual");
+echo $mercedes->getInfo();
+echo $mercedes->getTransmision();
+$bmx = new Bicicleta(2, 1);
+echo $bmx->getInfo();
