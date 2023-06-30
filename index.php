a6d29054f0dbb4795e32a8f508890400148d1f42
JonathanDavidAlvarezMonsalveCAMPUS <jonathandalvarez7@gmail.com>
2023-06-30 08:41:07 -0500

    feat: :sparkles: punto 22
    namespaces

--- index.php
+++ index.php
@@ -1,11 +1,13 @@
 <?php
-/*
- * Autoload 
+/**
+ * *Punto 20 Namespaces
  */
- function my_autoload($clase)
- {
-     require __DIR__ . '/clases/' . $clase . '.php';
- }
- spl_autoload_register('my_autoload');
- $detalles = new Detalles();
- $clientes = new Clientes();
+namespace App;
+
+class Detalles
+{
+    public function __construct()
+    {
+        echo "Desde la clase de Detalles.php";
+    }
+}
