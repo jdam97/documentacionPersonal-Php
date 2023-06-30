eb1092ea2100452c381e7ec60a25f43843f834e4
JonathanDavidAlvarezMonsalveCAMPUS <jonathandalvarez7@gmail.com>
2023-06-30 08:19:19 -0500

    feat: :sparkles: punto 14
    subo jsonencode y jsondecode

--- index.php
+++ index.php
@@ -1,104 +1,42 @@
 <?php
-//13.Funciones
-
 /**
- * todo Punto 13 Funciones
- * *Funciones definidas por el usuario
- * *son bloques de código que se pueden llamar y ejecutar en cualquier parte de un programa para realizar una tarea específica.
- * 
+ todo Punto 14 Json_encode y json_decode
  */
+     $producto = [
+         [
+             'nombre' => 'jonathansito',
+             'precio' => 10000,
+             'disponible' => true
+         ],
+         [
+             'nombre' => 'kfc',
+             'precio' => 5000,
+             'disponible' => true
+         ],
+         [
+             'nombre' => 'las tortugas ninja',
+             'precio' => 2000,
+             'disponible' => false
+         ]
+     ];
 
-/**
- * todo Punto 13 Funciones
- * *Funciones que no retornan valor
- * *en programación las funciones que no retornan ningun valor se denominan función "void". en PHP podemos definir una funcion void usando la palabra clave VOID en lugar de especificar un tipo de retorno.
- * 
- */
-
- //ejemplo
-
- declare (strict_types=1);
-
- function sumar (int $numero1 = 0, array $numero2):void{
-    echo $numero1 + $numero2;
- };
-
- sumar(10,[]);
-
-/**
- * todo Punto 13 Funciones
- * *Funciones que retornan valor
- * *en Php se pueden definir funciones que permiten recordar un valor al momento de finalizar la ejecución de todas las expresiones que se encuentran en su cuerpo principal, osea que retorna.
- * 
- */
-
-function usuarioAutenticado(bool $autenticado) : ? string{
-    if($autenticado){
-        return "el usuario está autenticado";
-    } 
-    else{
-        return null;
-    }
-
-}
-$usuario = usuarioAutenticado(false);
-
-echo $usuario;
-
-/**
- * *Funciones por variable
- */
-$fn = function(){
-    return true;
-};
-
-echo $fn(); 
-
-/**
- * *Funciones con use()
- */
-
-$nombre = "jonathan";
-$fn = function () use($nombre){
-    return $nombre;
-};
-
-echo $fn();
-
-
-/**
- * * obtener parametros sin pasarlos
- */
-
- function datos(){
-    print_r(func_get_args());
-
- };
- datos("Jonathan",23,true);
-
-/**
- * *Funcion cosas que miguel dijo que estaba dificil
-* *func_get_args() and func_get_arg()
-*/
-
-function datos1():void{
-    print_r(...func_get_args());
-};
-
-datos1(["nombre"=>"Camilo", "edad"=> 23, "vida"=> true]);
-
-
-/**
- * *extraer datos con ... y extract para arrays asociativos
- * 
- */
-
- function datos2(){
-    extract(...func_get_args());
-    echo $nombre;
-    echo $edad;
-    echo $casa;
-};
-datos2(["nombre"=> "Jonathan","edad"=> 23,"casa"=> true]);
-
- ?>
+     
+     /*
+      * *json decode
+      */
+     $json = '{"nombre":"Pablo","edad":16,"ciudad":"cepita"}';
+     $data = json_decode($json);
+     var_dump($data);
+     //json encode
+     /*
+      * json encode
+      */
+     var_dump($producto);
+     $json = json_encode($producto, JSON_UNESCAPED_UNICODE);
+     var_dump($json);
+     /*
+      * json decode a objeto o array asociativo
+      */
+     $json = '{"nombre":"Pablo","edad":16,"ciudad":"la esperanzada"}';
+     $data = json_decode($json); //devuelve un objeto
+     $data = json_decode($json, true); //devuelve un array asosiativo
