3981d1296ea01d7d38637f433af38f67330a71d3
JonathanDavidAlvarezMonsalveCAMPUS <jonathandalvarez7@gmail.com>
2023-06-30 08:34:25 -0500

    feat: :sparkles: punto 18
    clases abstractas

--- index.php
+++ index.php
@@ -1,41 +1,33 @@
 <?php
-/*
- * Herencia POO
+/**
+ * todo Clases abstractas
  */
 
-class Transporte
-{
-    public function __construct(protected int $ruedas, protected int $capacidad)
-    {
-    }
-    public function getInfo(): string
-    {
-        return "El transporte tiene " . $this->ruedas . " ruedas y una capacidad de " . $this->capacidad . " personas.";
-    }
-    public function getRuedas(): int
-    {
-        return $this->ruedas;
-    }
-}
-class Bicicleta extends Transporte
-{
-    public function getInfo(): string
-    {
-        return "El transporte tiene " . $this->ruedas . " ruedas y una capacidad de " . $this->capacidad . " personas Y NO GASTA GASOLINA.";
-    }
-}
-class Automovil extends Transporte
-{
-    public function __construct(protected int $ruedas, protected int $capacidad, protected string $transmision)
-    {
-    }
-    public function getTransmision()
-    {
-        return $this->transmision;
-    }
-}
-$mercedes = new Automovil(4, 5, "Manual");
-echo $mercedes->getInfo();
-echo $mercedes->getTransmision();
-$bmx = new Bicicleta(2, 1);
-echo $bmx->getInfo();
+ abstract class Animal
+ {
+     abstract public function hacerSonido();
+ }
+ class Capibara extends Animal
+ {
+     public function __construct()
+     {
+     }
+     public function hacerSonido()
+     {
+         echo "Glu, glu, glu.";
+     }
+ }
+ class Camello extends Animal
+ {
+     public function __construct()
+     {
+     }
+     public function hacerSonido()
+     {
+         echo "every go to the disco.";
+     }
+ }
+ $chompiras = new Capibara();
+ $tripaSeca = new Camello();
+ $chompiras->hacerSonido();
+ $tripaSeca->hacerSonido();
