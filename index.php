324b0ee8b31387158b2001e7b8c4ba4da4d461ca
JonathanDavidAlvarezMonsalveCAMPUS <jonathandalvarez7@gmail.com>
2023-06-30 08:39:49 -0500

    feat: :sparkles: punto 21
    autoload

--- index.php
+++ index.php
@@ -1,42 +1,11 @@
 <?php
-/**
- * *Polimorfismo
+/*
+ * Autoload 
  */
- interface TransporteInterfaz
+ function my_autoload($clase)
  {
-     public function getInfo(): string;
-     public function getRuedas(): int;
+     require __DIR__ . '/clases/' . $clase . '.php';
  }
- class Transporte implements TransporteInterfaz
- {
-     public function __construct(protected int $ruedas, protected int $capacidad)
-     {
-     }
-     public function getInfo(): string
-     {
-         return "El transporte cuenta con " . $this->ruedas . " ruedas y tiene una capacidad de " . $this->capacidad . " personas.";
-     }
-     public function getRuedas(): int
-     {
-         return $this->ruedas;
-     }
- }
- class Automovil extends Transporte implements TransporteInterfaz
- {
-     public function __construct(protected int $ruedas, protected int $capacidad, protected string $color)
-     {
-     }
-     public function getInfo(): string
-     {
-         return "El auto cuenta con " . $this->ruedas . " ruedas y tiene una capacidad de " . $this->capacidad . " personas. Aparte es de color " . $this->color;
-     }
-     public function getColor()
-     {
-         return "El color es: " . $this->color;
-     }
- }
- $transporte = new Transporte(8, 20);
- $bmw = new Automovil(4, 5, "Negro");
- echo $transporte->getInfo();
- echo $bmw->getInfo();
- echo $bmw->getColor();
+ spl_autoload_register('my_autoload');
+ $detalles = new Detalles();
+ $clientes = new Clientes();
